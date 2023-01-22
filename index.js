e4896c91a06b228299d33261cc899a4502c0a7fa
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-21 18:31:20 -0600

    finished a hard lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(footballStats) {
+    const winningStat = footballStats.find(football => football.result === 'W');
+    return winningStat ? winningStat.year : undefined
+    }
